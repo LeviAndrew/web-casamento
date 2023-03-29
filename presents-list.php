157d7448013d91ed8f164cb131b07a575d88b5b8
Levi Andrew <levi.andrew98@gmail.com>
2023-03-29 10:16:17 -0300

    Second commit

---
+++ presents-list.php
@@ -0,0 +1,257 @@
+<div class="main-slider">
+	<div class="display-table center-text">
+		<div class="display-table-cell">
+			<div class="slider-content">
+				
+				<h6 class="title" style="letter-spacing:1em">casamento</h6>
+				<h3 class="pre-title">Thami <i class="icon icon-heart"></i>Levi</h3>
+				<h1 class="title">Lista de Presentes</h1>
+				<a href="https://thamilevi.com.br"><i class="icon icon-home" style="margin:0 10px 0 0"></i>Home</a>
+				
+			</div><!-- slider-content-->
+		</div><!--display-table-cell-->
+	</div><!-- display-table-->
+</div><!-- main-slider -->
+
+
+<section class="regular-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				
+				<div class="content">
+				
+					<div class="img-wrapper"><img src="images/regular-content-1-1000x650.jpg" alt="Content Image"></div>
+					
+					<div class="quoto-area margin-top-btm">
+						<h3 class="quoto" style="line-height:1em;"><i class="icon icon-quote"></i>
+						Digno de honra entre todos seja o matrimônio.”</h3>
+						<h5 class="name" style="margin-top:5px;">Hebreus 13:4</h5>
+					</div>
+					
+					
+					<h4 class="margin-top-btm" align="center">Como contribuir com Nossa Lista:</h4>
+					<p align="center" style="color:#E45F74;">Observe abaixo os ítens que precisamos comprar, os valores são uma média, e ao final escolha nos abençoar com valor total do ítem ou parte dele e efetue <strong>Transferência Bancária</strong> ou use seu cartão pelo <strong>Sistema do PayPal</strong>.</p>
+					
+					<div class="col-sm-12">
+						<div class="margin-bottom display-table">
+							<div class="display-table-cell">
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/00-cozinha_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/00-cozinha.jpg" alt="Gallery Image"></a></li>
+										<li><small>Guarda-Roupa Rivieira 2 portas, R$ 2.100,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/03-panelas_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/03-panelas.jpg" alt="Gallery Image"></a></li>
+										<li><small>Cama box com baú casal + Colchão, R$ 1.200,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/01-microondas_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/01-microondas.jpg" alt="Gallery Image"></a></li>
+										<li><small>Geladeira/Refrigerador Electrolux Frost Free, R$ 1.900,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/02-fogao_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/02-fogao.jpg" alt="Gallery Image"></a></li>
+										<li><small>Fogão 4 bocas Electrolux, R$ 1.500,00</small></li>
+									</ul>
+								</div>
+								
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/04-sofa_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/04-sofa.jpg" alt="Gallery Image"></a></li>
+										<li><small>Mesa de jantar Berlin com 4 cadeiras, R$ 1.420,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/05-mesa_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/05-mesa.jpg" alt="Gallery Image"></a></li>
+										<li><small>Cozinha completa, R$ 2.500,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/06-lavadoura-roupas_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/06-lavadoura-roupas.jpg" alt="Gallery Image"></a></li>
+										<li><small>Máquina lavar storm wash midea 11kg, R$ 2.340,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/07-guarda-roupas_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/07-guarda-roupas.jpg" alt="Gallery Image"></a></li>
+										<li><small>Sofá cama 3 lugares, R$ 2.000,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/08-colchao_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/08-colchao.jpg" alt="Gallery Image"></a></li>
+										<li><small>Rack sala, R$ 1.000,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Tv smart 50” 4K LG 220v, R$ 2.456,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Conjunto de panelas brinox 8 peças, R$ 654,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>12. Aparelho de jantar chá 30 peças, R$ 300,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogos de copos 6 peças, R$ 44,90</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo de taças com jarra de vidro 7 peças, R$ 120,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo de 6 taças de vidro sobremesa, R$ 52,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Faqueiro aço inox 42 peças, R$ 150,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo de cama casal 6 peças, R$ 135,77</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo de toalhas banho 5 peças, R$ 76,49</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Ferro de passar a vapor, R$ 107,91</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Conjunto de assadeiras retangular, R$ 149,99</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Taça de gin tônica 600mL, R$ 55,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo de taças de cristal vinho, R$ 71,90</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Cafeteira elétrica, R$ 274,90</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Batedeira planetária, R$ 271,92</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Jogo americano, R$ 90,98</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Kit travesseiro 04 peças, R$ 179,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Toalha Mesa, R$ 30,00</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Tapete veludo, R$ 86,49</small></li>
+									</ul>
+								</div>
+								<div class="col-sm-3" style="float:left">
+									<ul class="braidmaids-name">
+										<li><a href="images/lista/09-geladeira_view.jpg" data-fluidbox><img class="margin-bottom" src="images/lista/09-geladeira.jpg" alt="Gallery Image"></a></li>
+										<li><small>Tapete beira de cama, R$ 56,90</small></li>
+									</ul>
+								</div>
+							</div><!-- display-table-cell -->
+						</div><!-- margin-bottom -->
+					</div><!-- col-sm-6 -->
+					
+					<hr>
+					
+					<div class="col-sm-12">
+						<div class="margin-bottom display-table">
+								
+								
+								
+								<div class="col-sm-4" style="float:right;margin-bottom:10px;">
+									<p align="center">Dados bancários do Noivo:<br>
+									- Banco <strong>BRADESCO</strong><br>
+									- Ag: <strong>0347</strong> | CC <strong>77485-5</strong><br>
+									- Chave Pix (CPF): <strong>010.978.203-86</strong></p>
+								</div>
+								
+								<div class="col-sm-4" style="float:right;margin-bottom:10px;">
+									<p align="center">Dados bancários da Noiva:<br>
+									- Banco <strong>INTER</strong><br>
+									- Ag <strong>0001</strong> | CC <strong>17198856-6</strong><br>
+									- Chave Pix (CPF): <strong>869.105.740-87</strong></p>
+								</div>
+								
+								<div class="col-sm-4" style="float:leftmargin-bottom:10px;">
+									<p align="center">Contribuição pelo <strong>PayPal</strong>:</p>
+									<form action="https://www.paypal.com/donate" method="post" target="_top">
+										<input type="hidden" name="hosted_button_id" value="..." />
+										<input type="image" src="images/presentear.png" border="0" name="submit" title="PayPal - presentei-nos com segurança!" alt="Presentei-nos com PayPal" style="margin:10px auto;display:block;" />
+										<img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1" />
+									</form>
+								</div>
+
+						</div>
+					</div>
+					
+				
+				</div><!-- content -->
+				
+			</div><!-- col-sm-12 -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- section -->
